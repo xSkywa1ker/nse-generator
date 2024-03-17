12ecbfdc0b5617382255cabc57a56b9f827ac663
skyw4lker <maxraidenxs@gmail.com>
2024-03-18 00:05:12 +0300

    добавил обработку всех необходимых протоколов

--- src/traffic_parser.cpp
+++ src/traffic_parser.cpp
@@ -27,6 +27,9 @@ int traffic_parser(const char *path_to_traffic, std::string ip_scanner,std::stri
     while ((packetData = pcap_next(handle, &header))) {
         if (header.caplen >= 14) {
             const uint16_t etherType = (packetData[12] << 8) | packetData[13];
+            if (etherType == 0x0806){
+                std::cout << "ARP";
+            }
             if (etherType == 0x0800) {
                 ip_header *ipHeader = (ip_header *)(packetData + 14);
                 bool is_scanner = true;
@@ -45,7 +48,17 @@ int traffic_parser(const char *path_to_traffic, std::string ip_scanner,std::stri
                 } else if (ipHeader->proto == 17) {
                     udp_header *udpHeader = (udp_header *)(packetData + 14 + ((ipHeader->ver_ihl & 0x0F) << 2));
                     std::cout << "UDP" << std::endl;
-                    manager(udpHeader, is_scanner, 17);
+                    if (udpHeader->source == 67 || udpHeader->dest == 68) {
+                        std::cout << "DHCP" << std::endl;
+                        manager(DHCPHeader, is_scanner, 67);
+                    } else {
+                        manager(udpHeader, is_scanner, 17);
+                    }
+                }
+                else if (ipHeader->proto == 2) {
+                    icmp_header *icmpHeader = (icmp_header *)(packetData + 14 + ((ipHeader->ver_ihl & 0x0F) * 4));
+                    std::cout << "ICMP" << std::endl;
+                    manager(icmpHeader, is_scanner, 2);
                 }
             }
         }
