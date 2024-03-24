cb8f97bb98570441bbcba7b08c64d371c7ca93e1
skyw4lker <maxraidenxs@gmail.com>
2024-03-24 12:41:22 +0300

    Добавил первую версию шаблона UDP

---
+++ src/udp_sample.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <vector>
+
+int clientSocket;
+
+// Структура для заголовка UDP пакета
+struct UdpHeader {
+    u_short sourcePort;
+    u_short destinationPort;
+    u_short length;
+    u_short checksum;
+};
+
+struct ReceivedPacket {
+    UdpHeader udpHeader;
+    int sourcePort;
+};
+
+std::vector<ReceivedPacket> receivedPackets;
+
+// Функция отправки UDP пакета
+void send_udp_packet(int source_port, int dest_port, const char* data, size_t dataLength) {
+    struct sockaddr_in serverAddress;
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    const char* ipAddress = "192.168.3.10";
+
+    if (clientSocket < 0) {
+        perror("Error creating socket");
+        return;
+    }
+
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(dest_port);
+    serverAddress.sin_addr.s_addr = inet_addr(ipAddress);
+
+    // Создание сокета
+    clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
+
+    // Отправка данных
+    sendto(clientSocket, data, dataLength, 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+}
+
+// Функция принятия UDP пакета
+void receive_udp_packet(int listen_port) {
+    struct sockaddr_in clientAddress;
+    socklen_t addrLen = sizeof(clientAddress);
+    char buffer[1024]; // Буфер для приёма данных
+
+    // Создание сокета
+    int serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
+
+    struct sockaddr_in serverAddress;
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddress.sin_port = htons(listen_port);
+
+    // Привязка сокета к адресу
+    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+    // Получение данных
+    recvfrom(serverSocket, buffer, sizeof(buffer), 0, (struct sockaddr*)&clientAddress, &addrLen);
+
+    // Обработка полученных данных
+    ReceivedPacket receivedPacket;
+    receivedPacket.sourcePort = ntohs(clientAddress.sin_port);
+    receivedPackets.push_back(receivedPacket);
+
+}
