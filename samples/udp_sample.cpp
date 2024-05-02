74edb37a338174bdd42ff60a1370c1e5fb2a7f3a
skyw4lker <maxraidenxs@gmail.com>
2024-05-02 10:34:07 +0300

    modified udp

--- samples/udp_sample.cpp
+++ samples/udp_sample.cpp
@@ -1,4 +1,12 @@
-int clientSocket;
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define MAX_PACKETS 100
 
 // Структура для заголовка UDP пакета
 struct UdpHeader {
@@ -15,25 +23,39 @@ struct ReceivedPacket {
 
 std::vector<ReceivedPacket> receivedPackets;
 
-_short calculate_checksum(const char* data, size_t length) {
-    u_long sum = 0;
+unsigned short calculate_checksum(const char* data, size_t length) {
+    unsigned long sum = 0;
     while (length > 1) {
-        sum += *((u_short*)data);
+        sum += *((unsigned short*)data);
         data += 2;
         length -= 2;
     }
     if (length > 0) {
-        sum += *((u_char*)data);
+        sum += *((unsigned char*)data);
     }
     while (sum >> 16) {
         sum = (sum & 0xFFFF) + (sum >> 16);
     }
     sum = ~sum;
-    return (u_short)sum;
+    return (unsigned short)sum;
 }
 
-void send_udp_packet(int source_port, int dest_port, size_t dataLength, const char* data) {
-    // Кодирование UDP заголовка
+void send_and_receive_udp_packet(int source_port, int dest_port, const char* source_ip, const char* dest_ip, size_t dataLength, const char* data) {
+    // Создание сокета
+    int clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (clientSocket < 0) {
+        perror("Error creating socket");
+        return;
+    }
+
+    // Заполнение структуры с информацией об адресе сервера
+    struct sockaddr_in serverAddress;
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(dest_port);
+    serverAddress.sin_addr.s_addr = inet_addr(dest_ip);
+
+    // Отправка UDP пакета
     UdpHeader udpHeader;
     udpHeader.sourcePort = htons(source_port);
     udpHeader.destinationPort = htons(dest_port);
@@ -50,49 +72,56 @@ void send_udp_packet(int source_port, int dest_port, size_t dataLength, const ch
     memcpy(buffer, &udpHeader, sizeof(UdpHeader)); // Обновляем заголовок с правильной контрольной суммой
 
     // Отправка пакета
-    struct sockaddr_in serverAddress;
-    memset(&serverAddress, 0, sizeof(serverAddress));
-    const char* ipAddress = "192.168.3.10";
+    struct sockaddr_in destAddress;
+    memset(&destAddress, 0, sizeof(destAddress));
+    destAddress.sin_family = AF_INET;
+    destAddress.sin_addr.s_addr = inet_addr(dest_ip);
 
-    if (clientSocket < 0) {
-        perror("Error creating socket");
+    sendto(clientSocket, buffer, sizeof(buffer), 0, (struct sockaddr*)&destAddress, sizeof(destAddress));
+
+    // Получение UDP пакетов
+    struct sockaddr_in clientAddress;
+    socklen_t addrLen = sizeof(clientAddress);
+    char receivedBuffer[1024]; // Буфер для приема данных
+
+    // Получение данных
+    int bytesReceived = recvfrom(clientSocket, receivedBuffer, sizeof(receivedBuffer), 0, (struct sockaddr*)&clientAddress, &addrLen);
+    if (bytesReceived > 0) {
+        // Обработка полученных данных
+        ReceivedPacket receivedPacket;
+        memcpy(&receivedPacket.udpHeader, receivedBuffer, sizeof(UdpHeader));
+        receivedPacket.sourcePort = ntohs(clientAddress.sin_port);
+        receivedPackets.push_back(receivedPacket);
+    }
+
+    // Закрытие сокета
+    close(clientSocket);
+}
+
+void receive_udp_packet() {
+    if (receivedPackets.empty()) {
+        std::cout << "No UDP packets received." << std::endl;
         return;
     }
 
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(dest_port);
-    serverAddress.sin_addr.s_addr = inet_addr(ipAddress);
-
-    clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
-
-    sendto(clientSocket, buffer, sizeof(buffer), 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    // Вывод полей первого пакета из массива
+    ReceivedPacket& receivedPacket = receivedPackets[0];
+    std::cout << "Source Port: " << ntohs(receivedPacket.sourcePort) << std::endl;
+    std::cout << "Destination Port: " << ntohs(receivedPacket.udpHeader.destinationPort) << std::endl;
+    std::cout << "Length: " << ntohs(receivedPacket.udpHeader.length) << std::endl;
+    std::cout << "Checksum: " << ntohs(receivedPacket.udpHeader.checksum) << std::endl;
 }
 
+int main() {
+    const char* data = "Hello, UDP!";
+    const int source_port = 12345; // Замените на нужный порт
+    const int dest_port = 54321; // Замените на нужный порт
+    const char* source_ip = "127.0.0.1"; // Замените на нужный IP адрес
+    const char* dest_ip = "127.0.0.1"; // Замените на нужный IP адрес
 
-// Функция принятия UDP пакета
-void receive_udp_packet(int listen_port) {
-    struct sockaddr_in clientAddress;
-    socklen_t addrLen = sizeof(clientAddress);
-    char buffer[1024]; // Буфер для приёма данных
+    // Отправка и получение UDP пакетов
+    send_and_receive_udp_packet(source_port, dest_port, source_ip, dest_ip, strlen(data), data);
+    receive_udp_packet();
 
-    // Создание сокета
-    int serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
-
-    struct sockaddr_in serverAddress;
-    memset(&serverAddress, 0, sizeof(serverAddress));
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-    serverAddress.sin_port = htons(listen_port);
-
-    // Привязка сокета к адресу
-    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-
-    // Получение данных
-    recvfrom(serverSocket, buffer, sizeof(buffer), 0, (struct sockaddr*)&clientAddress, &addrLen);
-
-    // Обработка полученных данных
-    ReceivedPacket receivedPacket;
-    receivedPacket.sourcePort = ntohs(clientAddress.sin_port);
-    receivedPackets.push_back(receivedPacket);
-
+    return 0;
 }
